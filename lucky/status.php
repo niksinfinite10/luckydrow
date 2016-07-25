9157e3b974aca520604262a5f80a7627d1a32dcf
niksinfinite10 <nikhil.surati@gmail.com>
2016-07-25 14:40:24 -0700

    atomated status change

---
+++ lucky/status.php
@@ -0,0 +1,14 @@
+<?php
+include_once('./connection.php');
+if(isset($_GET['status']))
+{
+	$status = $_GET['status'];
+	$sql = "UPDATE `status` SET `current_status`= ".$status." WHERE id = 1";
+	$result = mysql_query($sql);
+
+	if($result)
+		echo $status;
+	else
+		echo 0;
+}
+?>
